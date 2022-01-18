eeae2bf272894044f5e747c5216cb26d9853982f
cuevacelis <cuevacelis@hotmail.com>
2022-01-17 20:32:14 -0500

    problema resuelto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: ["Sacar la ropa", "Hacer la cama", "Leer un rato"],
+    };
+  }
+
+  addList = (e) => {
+    if (e.keyCode === 13) {
+      this.setState({ list: [...this.state.list, e.target.value] });
+      e.target.value = "";
+    }
+  };
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.list.map((e) => (
+              <li>{e}</li>
+            ))}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <input
+              type="text"
+              id="new-task"
+              onKeyUp={(e) => this.addList(e)}
+              placeholder="Ingresa una tarea y oprime Enter"
+            />
+          </form>
         </div>
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
